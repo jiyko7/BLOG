2a3396a99f5436e53aa9128cbf79c0e998c6fd82
Woong <jiyko7@kyonggi.ac.kr>
2022-09-17 16:26:17 +0900

    lombok 테스트
    lombok 테스트

--- src/main/java/com/woong/blog/test/HttpControllerTest.java
+++ src/main/java/com/woong/blog/test/HttpControllerTest.java
@@ -14,6 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest : ";
+	
+	//http://localhost:8080/http/lombok
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member  m = Member.builder().username("ssar").password("12345").email("ssar@nate.com").build();
+		System.out.println(TAG + "getter : " + m.getId());
+		m.setId(5000);
+		System.out.println(TAG + "setter : " + m.getId());
+		return "lombok test 완료";
+	}
+	
 	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없음
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
