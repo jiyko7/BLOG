c5b89ea10e43c069ac3718ec9578139d60bab237
Woong <jiyko7@kyonggi.ac.kr>
2022-09-17 15:09:43 +0900

    http 테스트
    http 테스트

---
+++ src/main/java/com/woong/blog/test/HttpControllerTest.java
@@ -0,0 +1,44 @@
+package com.woong.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//사용자가 요청하면 HTML파일을 응답 - @Controller
+
+// 사용자가 요청하면 데이터를 응답
+@RestController
+public class HttpControllerTest {
+	
+	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없음
+	//http://localhost:8080/http/get (select)
+	@GetMapping("/http/get")
+	public String getTest( Member m) { //id=1&username=ssar&password=1234&email=ssr@nate.com  //스프링부트의 MessageConverter 변환해줌
+		//@RequestParam을 매개변수로 사용
+		return "get 요청 : " + m.getId() +"," + m.getUsername()+ "," + m.getPassword() +"," + m.getEmail();
+	}
+	
+	//http://localhost:8080/http/post (insert)
+	@PostMapping("/http/post")
+	public String postTest(@RequestBody Member m) { //스프링부트의 MessageConverter 변환해줌
+		//@RequestBody를 매개변수로 사용
+		return "post 요청 : " + m.getId() +"," + m.getUsername()+ "," + m.getPassword() +"," + m.getEmail();
+	}
+
+	//http://localhost:8080/http/put (update)
+	@PutMapping("/http/put")
+	public String putTest(@RequestBody Member m) { //스프링부트의 MessageConverter 변환해줌
+		//@RequestBody를 매개변수로 사용
+		return "put 요청 : "+ m.getId() +"," + m.getUsername()+ "," + m.getPassword() +"," + m.getEmail();
+	}
+
+	//http://localhost:8080/http/delete (delete)
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete 요청";
+	}
+}
